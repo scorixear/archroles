6284d556ffac5bdc134d9781f05cd0680c8dfdff
Paul Keller <contact.keller.paul@gmail.com>
2022-07-10 18:37:31 +0200

    additional changes

--- src/config_example.ts
+++ src/config_example.ts
@@ -4,8 +4,8 @@ const version = JSON.parse(fs.readFileSync('package.json').toString()).version;
 export default {
   botPrefix: "/",
   version,
-  archDiscordId: "790198417506828299",
-  archKDiscordId: "995681684865421323",
+  archDiscordId: "200746010102726657",
+  archKDiscordId: "995648794093944832",
   archRequiredRoles: [["790198459843215362", "960239276476498010"]],
   archKRoles: ["995681996506419280"],
   bypassRole: "995682096532164648"
